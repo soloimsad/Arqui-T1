319f5f3a0e7692dbeefff633e1f95200f6a234c6
soloimsad <javier.gomeza@usm.cl>
2023-08-04 14:56:12 -0400

    Prueba

--- T1.py
+++ T1.py
@@ -1,3 +1,5 @@
-print("Hola mundo")
-print("poto")
-buenas culiao
+def float_to_32bits(linea):
+ return 
+with open('operaciones.txt','r') as archivo:
+    for linea in archivo:
+        ''' FUNCION TRANSFORMAR DE DECIMAL A IEEE754 '''
